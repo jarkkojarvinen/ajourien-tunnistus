cc43fd89ea45e87ebcb515725b0bb79a0bf5e845
Jarkko Järvinen <ext.jarkko.sa.jarvinen@valtion.onmicrosoft.com>
2021-03-05 16:57:39 +0200

    Python implementation added

--- pycode/src/main.py
+++ pycode/src/main.py
@@ -1,5 +1,7 @@
 import numpy as np
 import scipy.io as sio
+from skimage.filters.rank import entropy
+from skimage.morphology import disk, square
 from progress.bar import Bar
 from .initZs import init_Zs
 from .getDirectionalH import get_directional_H
@@ -47,6 +49,7 @@ def run(mat_fname, m=0.03292):
             indsLx = np.arange(dl[0], sz0[0], kSkip)
             indsLy = np.arange(dl[1], sz0[1], kSkip)
             z = z0[indsLx][:, indsLy]
+            sz = z.shape
 
             #n, m = np.shape(z)
             zs = init_Zs(z)
@@ -59,79 +62,43 @@ def run(mat_fname, m=0.03292):
             for k in range(0, nAlphas):
                 alpha = alphas[k]
                 H, s = get_directional_H(alpha, delta, z, zs)
-                # entropy = entropyfilt(H) # TODO Continue here
-                #data.append(DataStruct(H=H, J=entropy, s=s))
+                entr_img = entropy(H, square(9))  # entropy = entropyfilt(H)
+                data.append(DataStruct(H=H, J=entropy, s=s))
 
-    #     disp('assembling an image from entropy minimae');
-    #     Js= zeros(1,nAlphas); counter= 0; dCounter= 100000; nAll= prod(sz);
-    #     A= 0*H; % aspects
-    #     for i=1:sz(1)
-    #         for j=1:sz(2)
-    #             if mod(counter,dCounter) == 0
-    #                 disp(sprintf('%.1f %% done', (lFactor+counter/(nAll*nDls))*100));
-    #             end
-    #             counter= counter+1;
+            nAll = sz.prod(axis=1)
+            with Bar('Assembling an image from entropy minimae', max=nAll*nDls) as subbar:
+                Js = np.zeros(nAlphas)
+                counter = 0
+                dCounter = 100000
 
-    #             for k=1:nAlphas
-    #                 Js(k)= data(k).J(i,j);
-    #             end
-    #             [~,k]= min(Js);
-    #             H(i,j)= data(k).H(i,j);
-    #             A(i,j)= k;
-    # 			s(i,j)= data(k).s(i,j);
-    #         end
-    #     end
+                # aspects
+                A = 0 * H
+                for i in range(0, sz(0)):
+                    for j in range(0, sz(1)):
+                        if counter % dCounter == 0:
+                            subbar.next()
+                        counter = counter + 1
 
-    #     Hfinal(indsLx,indsLy)= H;
-    #     Afinal(indsLx,indsLy)= A;
-    # 	sFinal(indsLx,indsLy)= s;
-    #     indsXUsed= [indsXUsed, indsLx];
-    #     indsYUsed= [indsYUsed, indsLy];
-    # end
-    # indsXUsed= sort(unique(indsXUsed));
-    # indsYUsed= sort(unique(indsYUsed));
-    # H= Hfinal(indsXUsed,indsYUsed); clear Hfinal;
-    # A= Afinal(indsXUsed,indsYUsed); clear Afinal;
-    # s= sFinal(indsXUsed,indsYUsed); clear sFinal;
-    # mask= mask(indsXUsed,indsYUsed);
+                        for k in range(1, nAlphas):
+                            Js[k] = data[k].J[i, j]
 
-    # % mask the non-target zone away from the histograms
-    # m= reshape(mask,prod(size(mask)),1); inds= find(m);
-    # Htemp= reshape(H,prod(size(H)),1); Htemp= Htemp(inds);
-    # [fk,kappaBins]= hist(Htemp, 80); clear Htemp;
+                        k = min(Js)[1]
+                        H[i, j] = data[k].J[i, j]
+                        A[i, j] = k
+                        s[i, j] = data[k].s[i, j]
 
-    # % mask the margins away from the histograms
-    # fk= fk/trapz(kappaBins,fk); cfk= cumsum(fk)/sum(fk); eps= 0.05;
-    # [~,i1]= min(abs(cfk-eps)); [~,i2]= min(abs(cfk-(1-eps)));
-    # Hmin= kappaBins(i1); Hmax= kappaBins(i2); % 90 % of values within [Hmin,Hmax]
+                HFinal[indsLx][:, indsLy] = H
+                AFinal[indsLx][:, indsLy] = A
+                sFinal[indsLx][:, indsLy] = s
+                indsXUsed = np.concatenate((indsXUsed, indsLx))
+                indsYUsed = np.concatenate((indsYUsed, indsLy))
 
-    # sTemp= reshape(s,prod(size(s)),1); stemp= sTemp(inds);
-    # [fs,sBins]= hist(sTemp, 80);
-    # fs= fs/trapz(sBins,fs); cfs= cumsum(fs)/sum(fs); eps= 0.005;
-    # [~,i3]= min(abs(cfs-eps)); [~,i4]= min(abs(cfs-(1-eps)));
-    # sMin= sBins(i3); sMax= sBins(i4); % 90 % of values within [sMin,sMax]
-
-    # figure(1);
-    # 	subplot(1,2,1); semilogy(kappaBins,fk);
-    #     xlabel('\kappa (m^{-1}'); ylabel('freq (m)'); title('\kappa histogram');
-    # 	subplot(1,2,2); semilogy(sBins,fs);
-    #     xlabel('slope (1)'); ylabel('freq. (1)'); title('slope histogram');
-    #     set(findall(gcf,'-property','FontSize'),'FontSize',12);
-
-    # figure(2);
-    #     imshow(H); caxis([Hmin,Hmax]); h= colorbar;
-    #     h.Title.String= '\kappa (m^{-1})';
-    #     title(['curvature with \delta= ',sprintf('%.2f',delta), ' (m)']);
-
-    # figure(3);
-    #     imshow(A); caxis([min(min(A)),max(max(A))]); h= colorbar;
-    #     h.Title.String= 'k of \alpha_k (1...8)';
-    #     title('aspect  index k of the aspect \alpha_k');
-    #     set(findall(gcf,'-property','FontSize'),'FontSize',12);
-
-    # figure(4);
-    #     imshow(s); caxis([0.5,1.0]); % caxis([sMin,sMax]);
-    #     h= colorbar; h.YDir= 'reverse';
-    #     h.Title.String= 'slope (1)';
-    #     title(['slope with \delta= ',sprintf('%.2f',delta), ' (1)']);
-    #     set(findall(gcf,'-property','FontSize'),'FontSize',12);
+        indsXUsed = np.sort(np.unique(indsXUsed), axis=0)
+        indsYUsed = np.sort(np.unique(indsYUsed), axis=0)
+        H = HFinal[indsLx][:, indsLy]
+        Hfinal = None
+        A = AFinal[indsLx][:, indsLy]
+        Afinal = None
+        s = sFinal[indsLx][:, indsLy]
+        sFinal = None
+        #mask = mask(indsXUsed, indsYUsed)
