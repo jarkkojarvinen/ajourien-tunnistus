4b3d5a8314879b4bd15cf4cf5337a7dddda5310f
Jarkko Järvinen <ext.jarkko.sa.jarvinen@valtion.onmicrosoft.com>
2021-03-06 14:17:10 +0200

    Fix python code from main.py

--- pycode/src/main.py
+++ pycode/src/main.py
@@ -87,18 +87,71 @@ def run(mat_fname, m=0.03292):
                         A[i, j] = k
                         s[i, j] = data[k].s[i, j]
 
-                HFinal[indsLx][:, indsLy] = H
-                AFinal[indsLx][:, indsLy] = A
-                sFinal[indsLx][:, indsLy] = s
-                indsXUsed = np.concatenate((indsXUsed, indsLx))
-                indsYUsed = np.concatenate((indsYUsed, indsLy))
+            HFinal[indsLx][:, indsLy] = H
+            AFinal[indsLx][:, indsLy] = A
+            sFinal[indsLx][:, indsLy] = s
+            indsXUsed = np.concatenate((indsXUsed, indsLx)).astype(int)
+            indsYUsed = np.concatenate((indsYUsed, indsLy)).astype(int)
 
-        indsXUsed = np.sort(np.unique(indsXUsed), axis=0)
-        indsYUsed = np.sort(np.unique(indsYUsed), axis=0)
-        H = HFinal[indsLx][:, indsLy]
-        Hfinal = None
-        A = AFinal[indsLx][:, indsLy]
-        Afinal = None
-        s = sFinal[indsLx][:, indsLy]
-        sFinal = None
-        #mask = mask(indsXUsed, indsYUsed)
+    indsXUsed = np.sort(np.unique(indsXUsed), axis=0)
+    indsYUsed = np.sort(np.unique(indsYUsed), axis=0)
+    H = HFinal[indsLx][:, indsLy]
+    Hfinal = None
+    A = AFinal[indsLx][:, indsLy]
+    Afinal = None
+    s = sFinal[indsLx][:, indsLy]
+    sFinal = None
+    mask = mask[indsXUsed][:, indsYUsed]
+
+    # mask the non-target zone away from the histograms
+    #m = reshape(mask,prod(size(mask)),1); 
+    #inds = find(m)
+    #Htemp = reshape(H,prod(size(H)),1)
+    #Htemp= Htemp(inds);    
+    #[fk,kappaBins]= hist(Htemp, 80)
+    #Htemp = None
+
+    # mask the margins away from the histograms
+    #fk= fk/trapz(kappaBins,fk)
+    # cfk= cumsum(fk)/sum(fk)
+    # eps= 0.05; 
+    #[~,i1]= min(abs(cfk-eps))
+    # [~,i2]= min(abs(cfk-(1-eps)))
+    #Hmin= kappaBins(i1)
+    #Hmax= kappaBins(i2); # 90 % of values within [Hmin,Hmax]
+
+    #sTemp= reshape(s,prod(size(s)),1)
+    # stemp= sTemp(inds)
+    #[fs,sBins]= hist(sTemp, 80)
+    #fs= fs/trapz(sBins,fs)
+    # cfs= cumsum(fs)/sum(fs)
+    # eps= 0.005
+    #[~,i3]= min(abs(cfs-eps))
+    # [~,i4]= min(abs(cfs-(1-eps)))
+    #sMin= sBins(i3)
+    # sMax= sBins(i4) # 90 % of values within [sMin,sMax]
+
+    # figure(1); 
+    #     subplot(1,2,1); semilogy(kappaBins,fk); 
+    #     xlabel('\kappa (m^{-1}'); ylabel('freq (m)'); title('\kappa histogram');
+    #     subplot(1,2,2); semilogy(sBins,fs);
+    #     xlabel('slope (1)'); ylabel('freq. (1)'); title('slope histogram');
+    #     set(findall(gcf,'-property','FontSize'),'FontSize',12);
+
+    # figure(2);
+    #     imshow(H); caxis([Hmin,Hmax]); h= colorbar;
+    #     h.Title.String= '\kappa (m^{-1})';
+    #     title(['curvature with \delta= ',sprintf('%.2f',delta), ' (m)']);
+
+    # figure(3);
+    #     imshow(A); caxis([min(min(A)),max(max(A))]); h= colorbar;
+    #     h.Title.String= 'k of \alpha_k (1...8)';
+    #     title('aspect  index k of the aspect \alpha_k'); 
+    #     set(findall(gcf,'-property','FontSize'),'FontSize',12);
+
+    # figure(4);
+    #     imshow(s); caxis([0.5,1.0]); % caxis([sMin,sMax]); 
+    #     h= colorbar; h.YDir= 'reverse';
+    #     h.Title.String= 'slope (1)';
+    #     title(['slope with \delta= ',sprintf('%.2f',delta), ' (1)']); 
+    #     set(findall(gcf,'-property','FontSize'),'FontSize',12);
