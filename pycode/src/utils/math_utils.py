3502b43140de8e670f41400de0d2db62c1fb6383
Jarkko Järvinen <ext.jarkko.sa.jarvinen@valtion.onmicrosoft.com>
2021-03-09 17:18:33 +0200

    Add python implementation of matlab equivalent histogram

---
+++ pycode/src/utils/math_utils.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+def histogram(x, nbins):
+    """
+    Matlab hist(x, nbins) equivalent implementation.
+    Returns (counts, center).
+    Where counts=a row vector, counts, containing the number of elements in each bin,
+          centers=a row vector, centers, indicating the location of each bin center on the x-axis.
+
+    See details: https://se.mathworks.com/help/matlab/ref/hist.html
+    """
+    bins = np.linspace(np.min(x), np.max(x), nbins, endpoint=False)
+    delta = (bins[1] - bins[0])/2
+    count, bins = np.histogram(x, bins=bins)
+    bins = bins + delta
+    return count, bins
