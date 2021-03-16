b7906f0108700fa940d88c55a4632a5681d6acea
Jarkko Järvinen <ext.jarkko.sa.jarvinen@valtion.onmicrosoft.com>
2021-03-16 08:08:50 +0200

    Fix mutable method arguments

--- pycode/src/schemas/datastruct.py
+++ pycode/src/schemas/datastruct.py
@@ -3,13 +3,20 @@ class DataStruct():
     Data object to hold curvature, entropy and slope
     """
 
-    def __init__(self, H=[], J=[], s=[]):
+    def __init__(self, H=None, J=None, s=None):
         """
         H = Curvature,
         J = Entrory,
         s = slope
         """
-
+        if isinstance(H, type(None)):
+            H = []
         self.H = H
+
+        if isinstance(J, type(None)):
+            J = []
         self.J = J
+
+        if isinstance(s, type(None)):
+            H = s
         self.s = s
--- pycode/src/schemas/zstruct.py
+++ pycode/src/schemas/zstruct.py
@@ -3,8 +3,10 @@ class ZStruct():
     Represents Z item on array used in initZs
     """
 
-    def __init__(self, z=[]):
+    def __init__(self, z=None):
         """
         z = array of items
         """
+        if isinstance(z, type(None)):
+            z = []
         self.z = z
